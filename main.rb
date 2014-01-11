0d98f870b03ceb664a295ff0e97e860971d11d2e
Mike Stone <mikestone14@gmail.com>
2014-01-11 16:39:39 -0500

    Created visual representation of classroom

--- main.rb
+++ main.rb
@@ -45,9 +45,11 @@ def yield_output(output)
 end
 
 def yield_output_hash(hash)
+  names_array = []
   hash.each do |key, value|
-    puts "For #{key}: #{value}"
+    names_array << value
   end
+  names_array
 end
 
 # Run functions
@@ -55,4 +57,26 @@ seat_picker = add_picker
 yield_output seat_picker
 
 seats = assign_seats
-yield_output_hash seats
+names_array = yield_output_hash(seats)
+names_array = names_array.map do |group|
+  seat_group = group.join(' & ')
+  while seat_group.length < 17
+    seat_group = "#{seat_group} "
+  end
+  seat_group
+end
+
+#If a name group is < 17 characters add space until they are 17 characters long
+
+
+puts ""
+puts ""
+puts "                               [PROJECTOR SCREEN]          "
+puts ""
+puts "    #{names_array[0]}          |   PROJECTOR   |          #{names_array[1]}"
+puts ""
+puts "    #{names_array[2]}          #{names_array[3]}          #{names_array[4]}"
+puts ""
+puts "    #{names_array[5]}          #{names_array[6]}          #{names_array[7]}"
+puts ""
+puts ""
