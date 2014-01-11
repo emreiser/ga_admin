92ca2dde15e62bd9988c689d8ffe69161a85e031
Jordan Morano <jordan.morano@gmail.com>
2014-01-11 18:21:25 -0500

    add functionality to slogan

--- slogan.rb
+++ slogan.rb
@@ -1,29 +1,96 @@
-class WebDevelop < Human
-  attr_accessor :responsible, :confidence, :humility, :balance
+require 'pry'
 
-  def initialize
-    @responsible = false
-    @confidence = false
-    @humility = false
-    @balance = false
-  end
+module School
 
-  def collaborate(people)
-  end
+  class WebDeveloper # < Human
+    attr_accessor :responsible, :confidence, :humility, :balance, :name, :age, :gender, :awake
 
-  def transform(ideas)
-    reality = self + ideas
-    return reality
+    def initialize(name, gender, age)
+      @responsible = false
+      @confidence = false
+      @humility = false
+      @balance = false
+      @name = name
+      @gender = gender
+      @age = age
+      @awake = false
+    end
+
+    def collaborate(people)
+    end
+
+    def transform(ideas)
+      'reality = self and ideas'
+    end
+
+    def ga_wdi
+      @responsible = true
+      @confidence = true
+      @humility = true
+      @balance = true
+    end
+
+    def learn
+      @awake = true
+      while @awake == true
+        puts 'learning...learning...head is hurting... learning... need to sleep...'
+        binding.pry
+      end
+    end
+
   end
 
 end
 
+################# Main Functions #########################
 
-#instantiate web developer student
-Student = WebDeveloper.new
-
-# constantly learning
-while student.awake?
-  student.learn!
+def prompt_user(prompt)
+  puts prompt
+  gets.chomp
 end
 
+def create_student(name, age, gender)
+  School::WebDeveloper.new(name, age, gender)
+end
+
+def yield_output(output)
+  puts output
+end
+
+
+###### starter function
+def go_student_go
+
+  name = prompt_user 'What is your name?'
+  age = prompt_user('How old are you?').to_i
+  gender = prompt_user 'Are you male or female?'
+
+  student = create_student(name, age, gender)
+
+  output = "Hi. I am a GA-WDI student whose name is #{student.name}. I am #{age} years old and am a #{gender}"
+
+  yield_output(output)
+
+  current_status = "Currently, I am... responsible: #{student.responsible}, confident: #{student.confidence}, humble: #{student.humility}, and life balance: #{student.balance}."
+
+  yield_output(current_status)
+
+  reply = prompt_user 'Would you like to transform??'
+
+  student.ga_wdi if reply.downcase.index('y') != nil
+
+  new_status = "Now, I am... responsible: #{student.responsible}, confident: #{student.confidence}, humble: #{student.humility}, and life balance: #{student.balance}."
+
+  yield_output(new_status)
+
+  reply = prompt_user 'OK. Ready to learn?'
+
+  student.learn if reply.downcase.index('y') != nil
+
+  end_message = 'Way to go. Keep on going!!'
+
+  yield_output(end_message)
+
+end
+
+go_student_go
